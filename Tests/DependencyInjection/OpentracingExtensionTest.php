dc9621d7a71e5f447ae78cb8b100a905abb3bbce
Christian Alexander Wolf <mail@cawolf.de>
2020-01-09 16:25:34 +0100

    add missing test

---
+++ Tests/DependencyInjection/OpentracingExtensionTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Auxmoney\OpentracingBundle\Tests\DependencyInjection;
+
+use Auxmoney\OpentracingBundle\Factory\TracerFactory;
+use Auxmoney\OpentracingBundle\Tests\Mock\MockOpentracingExtension;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class OpentracingExtensionTest extends TestCase
+{
+    private $subject;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->subject = new MockOpentracingExtension();
+    }
+
+    public function testLoad(): void
+    {
+        $container = new ContainerBuilder();
+        $container->setParameter('kernel.debug', 'test');
+        $container->setParameter('kernel.project_dir', '/some/path/to/random-project');
+
+        $this->subject->load([], $container);
+
+        self::assertSame('unknown', $container->getParameterBag()->all()['env(HOSTNAME)']);
+        self::assertSame('random-project', $container->getParameterBag()->all()['env(AUXMONEY_OPENTRACING_PROJECT_NAME)']);
+        self::assertSame('mock value', $container->getParameter('mock_parameter'));
+        self::assertArrayHasKey('auxmoney_opentracing', $container->getAliases());
+        self::assertArrayNotHasKey(TracerFactory::class, $container->getDefinitions());
+    }
+}
