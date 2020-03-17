4ccfef089a4b51dc65ef6f8e5ccddee6cd88bc72
eddycharly <charles.breteche@agriconomie.com>
2020-03-17 18:44:03 +0100

    fix: Wrap calls to flush in try catch (#26)
    contributors: @eddycharly

--- Internal/PersistenceService.php
+++ Internal/PersistenceService.php
@@ -4,17 +4,26 @@ declare(strict_types=1);
 
 namespace Auxmoney\OpentracingBundle\Internal;
 
+use Exception;
+use Psr\Log\LoggerInterface;
+
 final class PersistenceService implements Persistence
 {
     private $tracer;
+    private $logger;
 
-    public function __construct(Opentracing $opentracing)
+    public function __construct(Opentracing $opentracing, LoggerInterface $logger)
     {
         $this->tracer = $opentracing->getTracerInstance();
+        $this->logger = $logger;
     }
 
     public function flush(): void
     {
-        $this->tracer->flush();
+        try {
+            $this->tracer->flush();
+        } catch (Exception $exception) {
+            $this->logger->warning(self::class . ': Failed to flush tracer : ' . $exception->getMessage());
+        }
     }
 }
