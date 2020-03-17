120320974997a744bfff4df11fb5b378b761a4fa
charles-edouard.breteche <charles.breteche@agriconomie.com>
2020-03-17 13:27:07 +0100

    wrap calls to flush in try catch

--- Internal/PersistenceService.php
+++ Internal/PersistenceService.php
@@ -4,17 +4,26 @@ declare(strict_types=1);
 
 namespace Auxmoney\OpentracingBundle\Internal;
 
+use Psr\Log\LoggerInterface;
+use ReflectionException;
+
 final class PersistenceService implements Persistence
 {
     private $tracer;
+    private $logger;
 
-    public function __construct(Opentracing $opentracing)
+    public function __construct(Opentracing $opentracing, LoggerInterface $logger)
     {
         $this->tracer = $opentracing->getTracerInstance();
+        $this->logger = $logger;
     }
 
     public function flush(): void
     {
-        $this->tracer->flush();
+        try {
+            $this->tracer->flush();
+        } catch (ReflectionException $exception) {
+            $this->logger->warning('Failed to flush tracer : ' . $exception->getMessage());
+        }
     }
 }
--- Tests/Internal/PersistenceServiceTest.php
+++ Tests/Internal/PersistenceServiceTest.php
@@ -8,6 +8,8 @@ use Auxmoney\OpentracingBundle\Internal\Opentracing;
 use Auxmoney\OpentracingBundle\Internal\PersistenceService;
 use Auxmoney\OpentracingBundle\Tests\Mock\MockTracer;
 use PHPUnit\Framework\TestCase;
+use Prophecy\Argument;
+use Psr\Log\LoggerInterface;
 
 class PersistenceServiceTest extends TestCase
 {
@@ -17,6 +19,7 @@ class PersistenceServiceTest extends TestCase
     {
         parent::setUp();
         $this->opentracing = $this->prophesize(Opentracing::class);
+        $this->logger = $this->prophesize(LoggerInterface::class);
     }
 
     public function testFlushSuccess(): void
@@ -24,11 +27,26 @@ class PersistenceServiceTest extends TestCase
         $mockTracer = new MockTracer();
         $mockTracer->startActiveSpan('operation name');
         $this->opentracing->getTracerInstance()->willReturn($mockTracer);
+        $this->logger->warning(Argument::type('string'))->shouldNotBeCalled();
 
-        $subject = new PersistenceService($this->opentracing->reveal());
+        $subject = new PersistenceService($this->opentracing->reveal(), $this->logger->reveal());
 
         $subject->flush();
 
         self::assertEmpty($mockTracer->getSpans());
+    }
+
+    public function testFlushThrowsException(): void
+    {
+        $mockTracer = $this->prophesize(MockTracer::class);
+        $mockTracer->flush()->willThrow(new RuntimeException('exception happened'));
+        $mockTracer->reveal()->startActiveSpan('operation name');
+
+        $this->opentracing->getTracerInstance()->willReturn($mockTracer->reveal());
+        $this->logger->warning(Argument::type('string'))->shouldBeCalled();
+
+        $subject = new PersistenceService($this->opentracing->reveal(), $this->logger->reveal());
+
+        $subject->flush();
     }
 }
