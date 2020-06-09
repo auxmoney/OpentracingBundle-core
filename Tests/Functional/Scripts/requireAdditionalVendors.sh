0477974d5a43865a379c85c42f795bb6829b5b57
Christian Alexander Wolf <mail@cawolf.de>
2020-06-09 07:38:18 +0200

    fix: hardcode major version for now

--- Tests/Functional/Scripts/requireAdditionalVendors.sh
+++ Tests/Functional/Scripts/requireAdditionalVendors.sh
@@ -3,6 +3,6 @@
 cd build/testproject/
 composer config extra.symfony.allow-contrib true
 composer config repositories.origin vcs https://github.com/${PR_ORIGIN}
-composer update auxmoney/core
+composer require auxmoney/opentracing-bundle-core:"dev-${BRANCH} as 0.6.7"
 composer require php-http/curl-client nyholm/psr7 webmozart/assert
 cd ../../
