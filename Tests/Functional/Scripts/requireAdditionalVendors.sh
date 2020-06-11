68d8993de77c3eb2cbc6c5c50454f74cfe4ed9d1
Christian Alexander Wolf <mail@cawolf.de>
2020-06-11 16:34:27 +0200

    fix: remove unnecessary steps

--- Tests/Functional/Scripts/requireAdditionalVendors.sh
+++ Tests/Functional/Scripts/requireAdditionalVendors.sh
@@ -3,7 +3,5 @@
 cd build/testproject/
 composer config extra.symfony.allow-contrib true
 composer config repositories.origin vcs https://github.com/${PR_ORIGIN}
-composer config use-github-api false
-composer require auxmoney/opentracing-bundle-core:"dev-${BRANCH} as 0.6.7"
 composer require php-http/curl-client nyholm/psr7 webmozart/assert
 cd ../../
