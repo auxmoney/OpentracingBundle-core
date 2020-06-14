3f07db9d9d3101ea55bf718354b81fb42c6331f2
Christian Alexander Wolf <mail@cawolf.de>
2020-06-14 20:25:16 +0200

    fix: use correct vendor version

--- Tests/Functional/Scripts/requireAdditionalVendors.sh
+++ Tests/Functional/Scripts/requireAdditionalVendors.sh
@@ -3,5 +3,8 @@
 cd build/testproject/
 composer config extra.symfony.allow-contrib true
 composer config repositories.origin vcs https://github.com/${PR_ORIGIN}
+composer config use-github-api false
+CORE_VERSION=`composer show auxmoney/opentracing-bundle-core | grep versions | grep -o -E '\*\ .+' | cut -d' ' -f2 | cut -d',' -f1`
+composer require auxmoney/opentracing-bundle-core:"dev-${BRANCH} as ${CORE_VERSION}"
 composer require php-http/curl-client nyholm/psr7 webmozart/assert
 cd ../../
