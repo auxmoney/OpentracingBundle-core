57d0bdfe1d69b2be44c08cf045cb414e83cd34a8
Christian Alexander Wolf <cawolf@users.noreply.github.com>
2020-12-24 09:50:37 +0100

    fix: replace Travis with GitHub (#51)

--- Tests/Functional/Scripts/checkEnvironment.sh
+++ Tests/Functional/Scripts/checkEnvironment.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 
-if [[ -z ${BRANCH} ]]
-then
-    echo "\$BRANCH is empty, please set it to the current development branch you want to test";
-    exit 1;
-fi
 if [[ -z ${SYMFONY_VERSION} ]]
 then
     echo "\$SYMFONY_VERSION is empty, please set it to the target symfony version you want to test against";
-    exit 2;
-fi
-if [[ -z ${PR_ORIGIN} ]]
-then
-    echo "\$PR_ORIGIN is empty, please set it to the current 'owner/repo' slug you want to test against";
-    exit 3;
+    exit 1;
 fi
 
 php -v
