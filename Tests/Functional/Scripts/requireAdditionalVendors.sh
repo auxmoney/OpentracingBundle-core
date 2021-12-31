b450f86541f694fa53e65b5f063baaca9a70eadd
Christian A. Wolf <mail@cawolf.de>
2021-12-31 18:43:55 +0100

    fix: add inline alias

--- Tests/Functional/Scripts/requireAdditionalVendors.sh
+++ Tests/Functional/Scripts/requireAdditionalVendors.sh
@@ -8,7 +8,7 @@ CURRENT_REF=${GITHUB_HEAD_REF:-$GITHUB_REF}
 CURRENT_BRANCH=${CURRENT_REF#refs/heads/}
 if [ "$CURRENT_BRANCH" != "master" ]; then
     composer config minimum-stability dev
-    VENDOR_VERSION=":dev-${CURRENT_BRANCH}"
+    VENDOR_VERSION=":\"dev-${CURRENT_BRANCH} as 1.99.0\""
 fi
 composer require auxmoney/opentracing-bundle-core${VENDOR_VERSION} auxmoney/opentracing-bundle-jaeger
 composer require php-http/curl-client nyholm/psr7 webmozart/assert
