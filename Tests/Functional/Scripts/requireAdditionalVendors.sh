9693e54d8b3e049c15284f2737d9c28207709499
Christian A. Wolf <mail@cawolf.de>
2021-12-31 18:39:59 +0100

    debug: remove debug echos

--- Tests/Functional/Scripts/requireAdditionalVendors.sh
+++ Tests/Functional/Scripts/requireAdditionalVendors.sh
@@ -10,43 +10,6 @@ if [ "$CURRENT_BRANCH" != "master" ]; then
     composer config minimum-stability dev
     VENDOR_VERSION=":dev-${CURRENT_BRANCH}"
 fi
-
-
-
-
-###
-echo "CURRENT_BRANCH: $CURRENT_BRANCH"
-if [[ $CURRENT_BRANCH -ne "master" ]]; then
-    echo "$CURRENT_BRANCH -ne \"master\""
-fi
-if [[ $CURRENT_BRANCH -eq "master" ]]; then
-    echo "$CURRENT_BRANCH -eq \"master\""
-fi
-if [[ $CURRENT_BRANCH != "master" ]]; then
-    echo "$CURRENT_BRANCH != \"master\""
-fi
-if [[ $CURRENT_BRANCH == "master" ]]; then
-    echo "$CURRENT_BRANCH == \"master\""
-fi
-if [[ "$CURRENT_BRANCH" -ne "master" ]]; then
-    echo "\"$CURRENT_BRANCH\" -ne \"master\""
-fi
-if [[ "$CURRENT_BRANCH" -eq "master" ]]; then
-    echo "\"$CURRENT_BRANCH\" -eq \"master\""
-fi
-if [[ "$CURRENT_BRANCH" != "master" ]]; then
-    echo "\"$CURRENT_BRANCH\" != \"master\""
-fi
-if [[ "$CURRENT_BRANCH" == "master" ]]; then
-    echo "\"$CURRENT_BRANCH\" == \"master\""
-fi
-echo "VENDOR_VERSION: $VENDOR_VERSION"
-exit 1
-###
-
-
-
-
 composer require auxmoney/opentracing-bundle-core${VENDOR_VERSION} auxmoney/opentracing-bundle-jaeger
 composer require php-http/curl-client nyholm/psr7 webmozart/assert
 composer dump-autoload
