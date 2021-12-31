913979a9f3e54789fc7532831ea98b3ad8760cfa
Christian A. Wolf <mail@cawolf.de>
2021-12-31 10:39:59 +0100

    debug: show more envs

--- Tests/Functional/Scripts/requireAdditionalVendors.sh
+++ Tests/Functional/Scripts/requireAdditionalVendors.sh
@@ -9,6 +9,7 @@ echo "GITHUB_HEAD_REF: $GITHUB_HEAD_REF"
 echo "GITHUB_REF: $GITHUB_REF"
 echo "CURRENT_REF (GITHUB_HEAD_REF:-GITHUB_REF): $CURRENT_REF"
 CURRENT_BRANCH=${CURRENT_REF#refs/heads/}
+echo "CURRENT_BRANCH: $CURRENT_BRANCH"
 if [[ $CURRENT_BRANCH -ne "master" ]]; then
     composer config minimum-stability dev
     VENDOR_VERSION=":dev-${CURRENT_BRANCH}"
