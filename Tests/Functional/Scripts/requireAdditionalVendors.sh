7f7e37066654d502c36bbcc4996971ae98f77142
Christian A. Wolf <mail@cawolf.de>
2021-12-31 18:35:57 +0100

    debug: test behaviour

--- Tests/Functional/Scripts/requireAdditionalVendors.sh
+++ Tests/Functional/Scripts/requireAdditionalVendors.sh
@@ -6,10 +6,47 @@ composer config extra.symfony.allow-contrib true
 VENDOR_VERSION=""
 CURRENT_REF=${GITHUB_HEAD_REF:-$GITHUB_REF}
 CURRENT_BRANCH=${CURRENT_REF#refs/heads/}
-if [[ "$CURRENT_BRANCH" != "master" ]]; then
+if [ "$CURRENT_BRANCH" != "master" ]; then
     composer config minimum-stability dev
     VENDOR_VERSION=":dev-${CURRENT_BRANCH}"
 fi
+
+
+
+
+###
+echo "CURRENT_BRANCH: $CURRENT_BRANCH"
+if [[ $CURRENT_BRANCH -ne "master" ]]; then
+    echo "$CURRENT_BRANCH -ne \"master\""
+fi
+if [[ $CURRENT_BRANCH -eq "master" ]]; then
+    echo "$CURRENT_BRANCH -eq \"master\""
+fi
+if [[ $CURRENT_BRANCH != "master" ]]; then
+    echo "$CURRENT_BRANCH != \"master\""
+fi
+if [[ $CURRENT_BRANCH == "master" ]]; then
+    echo "$CURRENT_BRANCH == \"master\""
+fi
+if [[ "$CURRENT_BRANCH" -ne "master" ]]; then
+    echo "\"$CURRENT_BRANCH\" -ne \"master\""
+fi
+if [[ "$CURRENT_BRANCH" -eq "master" ]]; then
+    echo "\"$CURRENT_BRANCH\" -eq \"master\""
+fi
+if [[ "$CURRENT_BRANCH" != "master" ]]; then
+    echo "\"$CURRENT_BRANCH\" != \"master\""
+fi
+if [[ "$CURRENT_BRANCH" == "master" ]]; then
+    echo "\"$CURRENT_BRANCH\" == \"master\""
+fi
+
+exit 1
+###
+
+
+
+
 composer require auxmoney/opentracing-bundle-core${VENDOR_VERSION} auxmoney/opentracing-bundle-jaeger
 composer require php-http/curl-client nyholm/psr7 webmozart/assert
 composer dump-autoload
