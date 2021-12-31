7f4d394a684941a785275a49f6b5e7b3b66a78c5
Christian A. Wolf <mail@cawolf.de>
2021-12-31 18:48:15 +0100

    fix: pin version

--- Tests/Functional/Scripts/requireAdditionalVendors.sh
+++ Tests/Functional/Scripts/requireAdditionalVendors.sh
@@ -8,8 +8,8 @@ CURRENT_REF=${GITHUB_HEAD_REF:-$GITHUB_REF}
 CURRENT_BRANCH=${CURRENT_REF#refs/heads/}
 if [ "$CURRENT_BRANCH" != "master" ]; then
     composer config minimum-stability dev
-    composer config extra.branch-alias.dev-$CURRENT_BRANCH "1.99.x-dev"
-    VENDOR_VERSION=":dev-${CURRENT_BRANCH}"
+    composer config extra.branch-alias.dev-$CURRENT_BRANCH "1.99.0"
+    VENDOR_VERSION=":1.99.0"
 fi
 composer require auxmoney/opentracing-bundle-core${VENDOR_VERSION} auxmoney/opentracing-bundle-jaeger
 composer require php-http/curl-client nyholm/psr7 webmozart/assert
