c37cb7aa4888eb97accfa4b5b66004092f83a7f7
Christian A. Wolf <mail@cawolf.de>
2022-01-03 07:55:36 +0100

    fix: quoting

--- Tests/Functional/Scripts/requireAdditionalVendors.sh
+++ Tests/Functional/Scripts/requireAdditionalVendors.sh
@@ -8,10 +8,9 @@ CURRENT_REF=${GITHUB_HEAD_REF:-$GITHUB_REF}
 CURRENT_BRANCH=${CURRENT_REF#refs/heads/}
 if [ "$CURRENT_BRANCH" != "master" ]; then
     composer config minimum-stability dev
-    VENDOR_VERSION=":\"dev-${CURRENT_BRANCH} as 1.99.0\""
+    VENDOR_VERSION=:"dev-${CURRENT_BRANCH} as 1.99.0"
 fi
-echo $VENDOR_VERSION
-composer require auxmoney/opentracing-bundle-core${VENDOR_VERSION} auxmoney/opentracing-bundle-jaeger
+composer require "auxmoney/opentracing-bundle-core${VENDOR_VERSION}" auxmoney/opentracing-bundle-jaeger
 composer require php-http/curl-client nyholm/psr7 webmozart/assert
 composer dump-autoload
 cd ../../
