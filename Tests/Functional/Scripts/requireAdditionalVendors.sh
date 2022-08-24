3384761eff9301a44b0e968393345db5a4d05a51
Christian A. Wolf <mail@cawolf.de>
2022-08-24 08:01:37 +0200

    fix: finally figured out functional test setup for forks

--- Tests/Functional/Scripts/requireAdditionalVendors.sh
+++ Tests/Functional/Scripts/requireAdditionalVendors.sh
@@ -8,10 +8,11 @@ CURRENT_REF=${GITHUB_HEAD_REF:-$GITHUB_REF}
 CURRENT_BRANCH=${CURRENT_REF#refs/heads/}
 if [ "$CURRENT_BRANCH" != "master" ]; then
     composer config minimum-stability dev
-    composer config repositories.fork vcs ${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}.git
+    composer config repositories.auxmoney vcs ${GITHUB_SERVER_URL}/${FORKED_REPOSITORY:-$GITHUB_REPOSITORY}
     VENDOR_VERSION=:"dev-${CURRENT_BRANCH} as 1.99.0"
 
 fi
+composer config github-oauth.github.com ${GITHUB_TOKEN}
 composer require "auxmoney/opentracing-bundle-core${VENDOR_VERSION}" auxmoney/opentracing-bundle-jaeger
 composer require php-http/curl-client nyholm/psr7 webmozart/assert
 composer dump-autoload
